c4fba5115927eb3f357d5c8a15075ef3968b236d
stopbullyingmepls <hliu0072@student.monash.edu>
2021-07-18 19:56:33 +1000

    00-start after practicing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  textColor = 'blue';
+  title = "Courses outline";
+  currentLesson = null;
   courseLessons = [
     { title: 'Hello Angular' },
     { title: 'Component Fundamentals' },
@@ -21,6 +23,14 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  updateColor(){
+    this.textColor = "salmon";
+    console.log('update color');
+  }
+  selectLesson(lesson){
+    this.currentLesson = lesson;
+    console.log("Current lesson: ", this.currentLesson)
   }
 
 }
